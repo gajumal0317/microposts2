b1e3e906be44793d21dae841f5f6488b4b4760e6
gajumal <kazukig0317@gmail.com>
2020-07-26 01:12:28 +0000

    favorite

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,18 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-
   get 'signup', to: 'users#new'
-  resources :users, only: [:index, :show, :new, :create] do
+  
+  resources :users, only: [:index, :show, :new, :create] do 
+    #user controllerで定義するアクション
     member do
       get :followings
       get :followers
+      get :likes
     end
-    
   end
+  #それぞれのcontrollerで定義するアクション
   resources :microposts, only: [:create, :destroy]
   resources :relationships, only: [:create, :destroy]
+  resources :favorites, only: [:create, :destroy]
 end
