072ce280d9ee3f414ba400fed4e71fcea448cf2b
gajumal <kazukig0317@gmail.com>
2020-08-02 10:58:18 +0000

    20200803_1

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -2,7 +2,7 @@ class ToppagesController < ApplicationController
   def index
     if logged_in?
       @micropost = current_user.microposts.build  # form_with 用
-      @microposts = current_user.feed_microposts.order(id: :desc).page(params[:page])
+      @microposts = current_user.feed_microposts.order(id: :desc).page(params[:page]).per(25)
     end
   end
 end
